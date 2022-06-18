20928446a437bb279bc8dd778c805a7501b5636d
ffayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-18 19:31:54 +0300

    Add files via upload

---
+++ untitled17.py
@@ -0,0 +1,12 @@
+talaba_0 = {
+    'ism':'akmal',
+    'familiya':'aliyev',
+    'yosh': 25,
+    'fakultet':'matematika',
+    'kurs': 4
+    }
+print(talaba_0.items())
+for kalit, qiymat in talaba_0.items():
+    print(f'Kalit: {kalit}')
+    print(f'qiymat: {qiymat}\n')
+    

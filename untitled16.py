4c300e8d00be665b5a0fe59e4837952943c9fced
ffayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-18 17:46:21 +0300

    Add files via upload

---
+++ untitled16.py
@@ -0,0 +1,6 @@
+Taomlar = {'Ali':'osh', 'Sardor':'manti', 'Alisher':'shashlik'}
+ali=Taomlar['Ali']
+sardor=Taomlar['Sardor']
+alisher=Taomlar['Alisher']
+
+print(f"Alining sevimli taomi {ali}, Sardorning sevimli taomi {sardor}, Alisherning sevimli taomi {alisher}")

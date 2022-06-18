13345e4c31c4ca7205707ce67564d56dff79036c
ffayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-18 17:45:55 +0300

    Add files via upload

---
+++ untitled14.py
@@ -0,0 +1,6 @@
+t_shaxslar = ["Imom Buxiriy", "Mirzo-Ulug'bek", "Hasan Basriy", "Fatih Mehmet Sultan"]
+
+z_shaxslar = ["Elon Musk","Fazliddin","Steave","Ahmet","Zubeyr"]
+a = t_shaxslar.pop(0)
+b = z_shaxslar.pop(0)
+print("Men TArixiy shaxslardan  " + a + " Zamonimiz insonlaridan " + b + " bilan ko'rishishni istardim " )

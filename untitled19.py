5470eddd57019a97ef9dd5f0be559133f463052c
ffayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-18 19:31:22 +0300

    Add files via upload

---
+++ untitled19.py
@@ -0,0 +1,27 @@
+mahsulotlar = {
+    'olma':15000,
+    'anjir':17000,
+    'shaftoli':22000,
+    'nok':28900,
+    'anor':32000
+    }
+#print(mahsulotlar.keys())
+
+#print("Do'kondagi mahsulotlar: ")
+#for mahsulot in mahsulotlar.keys():
+#    print(mahsulot.title())
+
+#bozorlik = ['anor', 'shaftoli', 'non', 'baliq']
+#for mahsulot in mahsulotlar:
+#    if mahsulot in bozorlik:
+#       print(f"{mahsulot.title()}  {mahsulotlar[mahsulot]} so'm")
+        
+#for buyum in bozorlik:
+#    if buyum not in mahsulotlar:
+#        print(f"Iltimos do'koningizga {buyum} ham olib keling") 
+
+#print("Do'konimizdagi mahsulotlar")
+#for mahsulot in sorted(mahsulotlar):
+#    print(mahsulot.title())
+
+

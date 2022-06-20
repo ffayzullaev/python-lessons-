72877a94a5a721b6bd6b0f66840af6775f28326f
Fazliddin Fayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-21 00:06:43 +0300

    Add files via upload

---
+++ untitled29.py
@@ -0,0 +1,8 @@
+print("Siz sevgan kitob... ")
+kitob = ("Kitobni nomini kiriting: ")
+kitob += "Dasturni to'tatish uchun 'stop' deb yozing "
+
+while True:
+    qiymat = input(kitob)
+    if qiymat == 'stop':
+        break 

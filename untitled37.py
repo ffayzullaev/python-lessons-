4b2a5a1e391f0188bd663a38c31a01a2401d1147
Fazliddin Fayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-22 00:05:34 +0300

    Add files via upload

---
+++ untitled37.py
@@ -0,0 +1,13 @@
+def juft(son):
+    if son%2:
+        print(f"{son} toq son")
+    else:
+        print(f"{son} juft son")
+        
+juft(10)
+juft(123)
+
+
+
+
+
---
+++ untitled38.py
@@ -0,0 +1,13 @@
+def sonlar(x,y):
+    """Solishtirish funksiyasi"""
+    if x > y:
+        print(f"{x}>{y}")
+    elif x<y :
+        print(f"{y}>{x}")
+    else:
+        print(f"{x}={y}")
+
+sonlar(1500, -5656) 
+sonlar(96, 100) 
+sonlar(6, 9) 
+sonlar(5, 5)     

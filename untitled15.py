37bd295283ed18d5fb4920777d67e25c6d5099db
Fazliddin Fayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-26 12:31:05 +0300

    Delete untitled15.py

--- untitled15.py
+++
@@ -1,8 +0,0 @@
-Otam = {'ism':'Yodgor', 'yosh':45, 'viloyat':'Samarqand'}
-Onam = {'ism':'Mashhura', 'yosh':44, 'viloyat':'samarqand'}
-yosh=Otam['yosh']
-vil=Otam['viloyat'] 
-yosh_1=Onam['yosh']
-vil_1=Onam['viloyat']
-print(f"Otamning ismi {Otam['ism'].title()}, {yosh} yoshda, {vil.title()} viloyatda tug'ilgan")
-print(f"Onamning ismi {Onam['ism'].title()}, {yosh_1} yoshda, {vil_1.title()} viloyatda tug'ilgan")

53fbcca0c4eb40cac59a04df4ca03002b3b6381c
Fazliddin Fayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-26 12:33:50 +0300

    Delete untitled29.py

--- untitled29.py
+++
@@ -1,8 +0,0 @@
-print("Siz sevgan kitob... ")
-kitob = ("Kitobni nomini kiriting: ")
-kitob += "Dasturni to'tatish uchun 'stop' deb yozing "
-
-while True:
-    qiymat = input(kitob)
-    if qiymat == 'stop':
-        break 

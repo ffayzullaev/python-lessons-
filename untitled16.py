9b064b56e5b03e0c99eda743517dd05721dc11c5
Fazliddin Fayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-06-26 12:31:16 +0300

    Delete untitled16.py

--- untitled16.py
+++
@@ -1,6 +0,0 @@
-Taomlar = {'Ali':'osh', 'Sardor':'manti', 'Alisher':'shashlik'}
-ali=Taomlar['Ali']
-sardor=Taomlar['Sardor']
-alisher=Taomlar['Alisher']
-
-print(f"Alining sevimli taomi {ali}, Sardorning sevimli taomi {sardor}, Alisherning sevimli taomi {alisher}")

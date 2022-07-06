fbef0877409cfd0c9e13f77fb6992298b80859f4
Fazliddin Fayzullaev <107722921+ffayzullaev@users.noreply.github.com>
2022-07-06 03:18:39 +0300

    Add files via upload

---
+++ Python asoslari./43-dars_avto.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 03:09:16 2022
+
+author: Fazliddin Fayzullaev
+"""
+from  avto import Avto
+avto1 = Avto("GM", "Nexia3", "Oq", 2021, 18000)
+avto2 = Avto("GM", "Malibu", "Qora", 2022, 42000)
+avto3 = Avto("Gm", "Captiva", "Oq", 2021, 45000)
+print(Avto.get_num_avto())
